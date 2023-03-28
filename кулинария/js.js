721db6c0e7d85aed8b3b3784005cfcdbfcf64259
kostanyc <129169808+kostanyc@users.noreply.github.com>
2023-03-28 16:44:03 +0600

    Add files via upload

---
+++ кулинария/js.js
@@ -0,0 +1,11 @@
+let button =document.getElementById("button")
+let body =document.querySelector("body")
+let color =["black"]
+
+body.style.backgroundColor="black"
+body.style.color ="black"
+
+button.addEventListener("click",function(){
+   
+    body.style.backgroundColor= "black"
+})
